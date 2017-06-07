cb433ede8a035be7050244a3a2c887ce7b700315
andrew.morgan <andrew.morgan@capp.co>
2017-06-07 19:45:08 +0100

    Add project files.

---
+++ Sample/Controllers/Sample01Controller.cs
@@ -0,0 +1,28 @@
+﻿using Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class Sample01Controller : Controller
+    {
+        // GET: Sample01
+        public ActionResult Index()
+        {
+            var m = new Sample01ViewModel();
+            return View(m);
+        }
+
+        // POST: sample01
+        [HttpPost]
+        public ActionResult Index(string dummy)
+        {
+            var m = new Sample01ViewModel();
+            m.SelectedCountry = Request["ddlCountries"];
+            return View(m);
+        }
+    }
+}
---
+++ Sample/Models/Sample01ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Models
+{
+    public class Sample01ViewModel
+    {
+        public string SelectedCountry { get; set; }
+        public List<Country> countries
+        {
+            get
+            {
+                return new List<Country>()
+                {
+                    new Country() {Id = "IN", Name = "India"},
+                    new Country() {Id = "FN", Name = "France"},
+                    new Country() {Id = "EN", Name = "England"},
+                    new Country() {Id = "IT", Name = "Italy"},
+                    new Country() {Id = "GR", Name = "Germany"},
+                    new Country() {Id = "ES", Name = "Spain"},
+                    new Country() {Id = "US", Name = "United States Of America"}
+                };
+                
+            }
+        }
+
+        public IEnumerable<SelectListItem> GetCountriesListItems()
+        {
+            return countries.Select(c => new SelectListItem()
+            {
+                Text = c.Id + " - " + c.Name,
+                Value= c.Id,
+                Selected = (c.Id == SelectedCountry)
+            });
+        }
+    }
+}
